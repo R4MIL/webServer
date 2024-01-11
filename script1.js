15698b25f3e8e7985cd76ab7a013998147d3782d
R4MIL <147025608+R4MIL@users.noreply.github.com>
2024-01-11 23:27:21 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+const username = 'Ramil'
+const age = 32
+export const data = {
+    username,
+    age
+}
+
+const defaultValue = 'export default'
+export default {
+    defaultValue
+}
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+import {data} from './script1.js'
+import dataDefault from './script1.js'
+
+console.log(data);
+console.log(dataDefault);
